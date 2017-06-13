3ed8767437ac2ec30701e97cf8c8dcc20cbab979
gabrielpereirapinheiro <140020764@aluno.unb.br>
2017-06-13 14:46:24 -0300

    middle

--- karatsuba.c
+++ karatsuba.c
@@ -17,19 +17,83 @@ int show_message(int number)
 	return value;
 }
 
-int karatusa(int n1,int n2, int base)
+int base(int number)
 {
+	int n=10;
+
+	while(n<number)
+	{
+		n=n*10;
+	}
+	return n/10;		
+}
+
+int maximum(int number,int b)
+{
+	int m=1;
+
+	while(m*b<number)
+	{
+		m=m+1;
+	}
+
+	return m-1;
+
+}
+
+int rest(int number,int b,int m)
+{
+	int rest = number - b*m;
+}
+
+int karatusa(int n1,int n2)
+{
+	if( n1 <10 || n2<10)
+	{
+		return n1*n2;
+	}
+
+	int z0,z1,z2;
+	int z2_1,z2_2;
+	int b1,b2;
+
+	b1 = base(n1);
+	b2 = base(n2);
+
+	if(b1>b2)
+	{
+		b1=b2;
+	}
+	else
+	{
+		b2=b1;
+	}
+
+	z2_1 = maximum(n1,b1);
 	
+	z2_2 = maximum(n2,b2);
+
+	z2 = z2_1 * z2_2;
+
+	z0 = rest(n1,b1,z2_1) * rest(n2,b2,z2_2);
+
+	z1 = (z2_1+rest(n1,b1,z2_1))*(z2_2+rest(n2,b2,z2_2));
+
+	z1 = z1 - z2 - z0;
+
+	return z1;
 }
 
 
 int main()
 {
-	int number1, number2,base=10;
+	int number1, number2;
 
 	number1 = show_message(1);
 	number2 = show_message(2);
 
-	karatusa(number1,number2,base);
+	int a = karatusa(number1,number2);
+		
+	printf("%d\n",  a );
 	return 0;
 }
